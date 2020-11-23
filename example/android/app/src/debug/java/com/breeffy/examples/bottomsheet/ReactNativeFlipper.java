992b1b48733855f3cfa323f9f88efbc5dfae3bd0
Victor Malov <victor.malov@breeffy.com>
2020-11-23 19:04:18 +0300

    refactor: change example application native package name

--- example/android/app/src/debug/java/com/breeffy/examples/bottomsheet/ReactNativeFlipper.java
+++ example/android/app/src/debug/java/com/breeffy/examples/bottomsheet/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package dev.gorhom.bottomsheet;
+package com.breeffy.examples.bottomsheet;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
--- example/android/app/src/main/java/com/breeffy/examples/bottomsheet/MainApplication.java
+++ example/android/app/src/main/java/com/breeffy/examples/bottomsheet/MainApplication.java
@@ -1,4 +1,4 @@
-package dev.gorhom.bottomsheet;
+package com.breeffy.examples.bottomsheet;
 
 import android.app.Application;
 import android.content.Context;
@@ -67,7 +67,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("dev.gorhom.bottomsheet.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.breeffy.examples.bottomsheet.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
